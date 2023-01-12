faf888f26ab947a812a4490b67c8075a622f2f18
Yeeun Chung <yesung00000@gmail.com>
2023-01-12 20:56:57 +0900

    양과 늑대 어렵다...

--- src/com/company/programmers/etc/Main_양과늑대.java
+++ src/com/company/programmers/etc/Main_양과늑대.java
@@ -1,27 +1,23 @@
 package com.company.programmers.etc;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main_양과늑대 {
 
     public static void main(String[] args) {
 
-        int[] info = {0,0,1,1,1,0,1,0,1,0,1,1};
-        int[][] edges = {{0,1}, {1,2}, {1,4}, {0,8}, {8,7}, {9,10}, {9,11}, {4,3}, {6,5}, {4,6}, {8,9}};
-        System.out.println(solution(info, edges));
+//        int[] info = {0,0,1,1,1,0,1,0,1,0,1,1};
+//        int[][] edges = {{0,1}, {1,2}, {1,4}, {0,8}, {8,7}, {9,10}, {9,11}, {4,3}, {6,5}, {4,6}, {8,9}};
+//        System.out.println(solution(info, edges));
 
-//        int[] info2 = {0,1,0,1,1,0,1,0,0,1,0};
-//        int[][] edges2 = {{0,1}, {0,2}, {1,3}, {1,4}, {2,5}, {2,6}, {3,7}, {4,8}, {6,9}, {9,10}};
-//        System.out.println(solution(info2, edges2));
+        int[] info2 = {0,1,0,1,1,0,1,0,0,1,0};
+        int[][] edges2 = {{0,1}, {0,2}, {1,3}, {1,4}, {2,5}, {2,6}, {3,7}, {4,8}, {6,9}, {9,10}};
+        System.out.println(solution(info2, edges2));
 
     }
 
     public static Map<Integer, List<Integer>> edgeMap = new HashMap<>();
-    public static List<Integer> sheepList = new ArrayList<>();
-    public static int[] sheepCnt = new int[2]; // index 0: 양, 1: 늑대
+    public static int answer = 0; // 최댓값
 
     public static int solution(int[] info, int[][] edges) {
 
@@ -44,36 +40,30 @@ public class Main_양과늑대 {
 
         System.out.println(edgeMap);
 
-        sheepList.add(0);
-        sheepCnt[0]++;
+        List<Integer> nextList = new ArrayList<>(Arrays.asList(0));
+        int[] sheepCnt = new int[2]; // index 0: 양의 수, 1: 늑대의 수
 
-        dfs(0, info);
+        dfs(0, info, sheepCnt, nextList);
 
         return sheepCnt[0];
     }
 
 
-    static void dfs(int parent, int[] info) {
+    static void dfs(int parent, int[] info, int[] sheepCnt, List<Integer> nextList) {
 
-        System.out.println(parent);
-        List<Integer> childList = edgeMap.get(parent);
-
-        for (Integer child: childList) {
-            if (info[child] == 0) {       // 1. 양 만났을 때
-                info[child] = 2;          //// 이미 먹었다고 표시
-                sheepCnt[0]++;
-            } else if (info[child] == 1){ // 2. 늑대 만났을 때
-                if (sheepCnt[0] > sheepCnt[1] + 1) { // 지나가도 영향 X
-                    sheepCnt[1]++;
-                    dfs(child, info);
-                    sheepCnt[1]--;
-                } else { // 지나가면 영향이 있음
-                    continue;
-                }
-            } else {                      // 3. 비어있을 때
-                dfs(child, info); // 그냥 밑으로 내려감
-            }
+        if (info[parent] == 0) {
+            sheepCnt[0]++;
+        } else {
+            if (sheepCnt[1] + 1 == sheepCnt[0])
+                return;
+            else
+                sheepCnt[1]++;
         }
+
+        nextList.remove(parent);
+        nextList.addAll(edgeMap.getOrDefault(parent, new ArrayList<>()));
+        nextList.forEach(next -> );
+
 
     }
 
