b9ca843c412f064f65cf9ea7f9540c9aaad7faba
Yeeun Chung <yeeunchung@Yeeunui-MacBookPro.local>
2023-01-26 13:10:39 +0900

    [CYE] 미로탈출명령어

--- src/com/company/programmers/math/Main_미로탈출명령어.java
+++ src/com/company/programmers/math/Main_미로탈출명령어.java
@@ -1,21 +1,18 @@
-package com.company.programmers.etc;
-
-import java.util.Arrays;
+package com.company.programmers.math;
 
 public class Main_미로탈출명령어 {
 
     public static void main(String[] args) {
 
-        System.out.println(solution(3, 4, 2, 3, 3, 1, 5));
+//        System.out.println(solution(3, 4, 2, 3, 3, 1, 5));
 //        System.out.println(solution(2, 2, 1, 1, 2, 2, 2));
-//        System.out.println(solution(3, 3, 1, 2, 3, 3, 4));
+        System.out.println(solution(3, 3, 1, 2, 3, 3, 4));
 
     }
 
     static int[] dx = {1, 0, 0, -1};
     static int[] dy = {0, -1, 1, 0};
     static char[] alp = {'d', 'l', 'r', 'u'};
-    static String shortest;
 
     public static String solution(int n, int m, int x, int y, int r, int c, int k) {
 
@@ -29,28 +26,35 @@ public class Main_미로탈출명령어 {
          * lr
          *
          * 답: dll, dllrl
+         *
+         * . . . .
+         * . . S .
+         * E . . .
+         *
+         * 1 2 0 0
          */
 
-        
+        int[] count = new int[4]; // dlru count
+        String answer = "";
+
+        int width = c - y;
+        int height = r - x;
+
+        if (width > 0) count[2] += width;
+        else count[1] -= width;
+        if (height > 0) count[0] += height;
+        else count[3] -= height;
+
+        int shortest = Math.abs(width) + Math.abs(height);
 
         // 홀수만큼 남으면 impossible
-        if (shortest.length() > k || (k - shortest.length()) % 2 != 0) return "impossible1";
+        if (shortest > k || (k - shortest) % 2 != 0) return "impossible";
 
         // 짝수면 계산
-        int remain = (k - shortest.length()) / 2; // remain * 2만큼 더 추가해줘야 한다.
-        int[] count = new int[4]; // dlru count
-        for (int i=0; i<shortest.length(); ++i) {
-            switch (shortest.charAt(i)) {
-                case 'd': count[0]++; break;
-                case 'l': count[1]++; break;
-                case 'r': count[2]++; break;
-                case 'u': count[3]++; break;
-            }
-        }
+        int remain = (k - shortest) / 2; // remain * 2만큼 더 추가해줘야 한다.
 
         int currentX = x-1;
         int currentY = y-1;
-        String answer = "";
 
         for (int i=0; i<k; ++i) {
             int newX = -1, newY = -1;
@@ -60,10 +64,10 @@ public class Main_미로탈출명령어 {
                 newY = currentY + dy[j];
 
                 // 이동할 수 있으면 이동
-                if (newX >= 0 && newX < map.length && newY >= 0 && newY < map[0].length) {
+                if (newX >= 0 && newX < n && newY >= 0 && newY < m) {
                     if (count[j] > 0) {
                         count[j]--;
-                        answer.concat(String.valueOf(alp[j]));
+                        answer = answer.concat(String.valueOf(alp[j]));
                         break;
                     } else {
                         if (remain == 0) continue;
@@ -76,7 +80,7 @@ public class Main_미로탈출명령어 {
                         }
 
                         remain--;
-                        answer.concat(String.valueOf(alp[j]));
+                        answer = answer.concat(String.valueOf(alp[j]));
                         break;
                     }
                 }
@@ -86,7 +90,7 @@ public class Main_미로탈출명령어 {
                 currentX = newX;
                 currentY = newY;
             } else { // 어느 방향으로도 이동할 수 없었다
-                return "impossible2";
+                return "impossible";
             }
         }
 
